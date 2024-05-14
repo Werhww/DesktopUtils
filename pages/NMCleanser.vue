584a55cb34d3e651ce29e5e5cf7823e64a230a6c
werhw <kleppeleo@gmail.com>
2024-05-14 14:05:06 +0200

    Update NMCleanser.vue to dynamically generate a tree structure of node_modules folders and display it in a QTree component

--- pages/NMCleanser.vue
+++ pages/NMCleanser.vue
@@ -1,31 +1,56 @@
 <script setup lang="ts">
 import { invoke } from "@tauri-apps/api/tauri"
-
 import FolderSearch from "@/assets/svg/folder-search.svg"
 
-const selected = ref()
 const ticked = ref([])
 const expanded = ref([])
 
-const simple = [
-	{
-		label: "C:",
-		children: [
-			{
-				label: "/code",
-				children: [
-					{ label: "/constructopia" },
-					{ label: "/kys-pizzakalulator" },
-				],
-			},
-		],
-	},
-]
+const node_modules_tree = ref<any[]>([])
 
 async function searchTest() {
-	const test = await invoke("list_node_modules")
-	console.log(test)
+	const node_modules = await invoke("list_node_modules") as string[]
+	console.log(node_modules)
+	let tree: any[] = []
+
+	node_modules.forEach((path) => {
+		const pathArr = path.split(/[\\\/]/)
+		let current: any = tree
+
+		pathArr.forEach((folder, index) => {
+			if(folder === "node_modules") return
+
+			if (!current.find((node: any) => node.label === folder)) {
+				const content = index === pathArr.length - 2 ? {
+					label: folder,
+					folderPath: path,
+					icon: "folder",
+				} : index === 0 ? {
+					label: folder,
+					icon: "storage",
+				} : {
+					label: folder,
+					icon: "account_tree"
+				}
+				const node = { ...content, children: [] }
+				current.push(node)
+			}
+
+			if (index !== pathArr.length - 2) {
+				current = current.find((node: any) => node.label === folder).children
+			}
+
+		})
+	})
+
+	console.log(tree)
+	node_modules_tree.value = tree
 }
+
+watchEffect(() => {
+	console.log("checked: ", ticked.value)
+	console.log("expaneded: ", expanded.value)
+})
+
 </script>
 
 <template>
@@ -56,13 +81,19 @@ async function searchTest() {
 		class="col-12 col-sm-6"
 		tick-strategy="leaf"
 		node-key="label"
-		:nodes="simple"
+		:nodes="node_modules_tree"
 		control-color="grey-8"
 		:duration="100"
-		v-model:selected="selected"
 		v-model:ticked="ticked"
 		v-model:expanded="expanded"
-	/>
+	>
+		<template v-slot:default-header="prop">
+			<QIcon :name="prop.node.icon"/>
+			<div class="q-pl-xs">{{ prop.node.label }}</div>
+			
+		</template>
+
+	</QTree>
 </template>
 
 <style scoped lang="scss"></style>
